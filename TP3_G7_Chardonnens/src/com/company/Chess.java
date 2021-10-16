4f61ae2bb67e2c4cbdb01256819ceb4dd2a2eb12
thomas <chardonnens.thomas@protonmail.com>
2021-10-17 01:01:04 +0200

    initialise fully bug-free

--- TP3_G7_Chardonnens/src/com/company/Chess.java
+++ TP3_G7_Chardonnens/src/com/company/Chess.java
@@ -1,7 +1,6 @@
 package com.company;
 
-import com.sun.source.tree.BinaryTree;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Chess {
@@ -41,84 +40,103 @@ public class Chess {
 
     private void initialiseBoard() {
 
-        //Initialisation du plateau
-        board = new Cell['h'][8];
+        // Initialisation du plateau
+        // Initialisation de toutes les cases du plateau
+        // On crée chaque case du plateau puis on lui attribue une position dans l'échiquier
+        board = new Cell[8][8];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                board[i][j] = new Cell();
+            }
+        }
 
-        //Initialisation de toutes les cases du plateau
-        Cell[] cell = new Cell[64];
-        Position[] position = new Position[64];
+        Position[][] position = new Position[8][8];
+        for(int i = 0; i < position.length; i++){
+            for(int j = 0; j < position[i].length; j++){
+                position[i][j] = new Position();
+            }
+        }
+
         char columnChar = 'a';
-        int iteration = 0;
-        for (int row = 8; row >= 1; row--) { // Ligne de l'échiquier
-            for (int column = 1; column <= 8; column++) { // Colonne de l'échiquier
-                position[iteration] = new Position();
-                cell[iteration] = new Cell();
-                cell[iteration].init(position[iteration], true, null);
+        int row = 8;
+
+        for (int i = 0; i <= 7; i++) { // Lignes de l'échiquier
+            row -= i;
+             for (int j = 0; j <= 7; j++) { // Colonnes de l'échiquier
+                position[i][j].init(columnChar, row);
+                board[i][j].init(position[i][j], true, null);
                 columnChar += 1;
-                iteration += 1;
             }
             columnChar = 'a';
         }
 
-
         //Initialisation de toutes les pièces du plateau
         //Initialisation des pièces blanches
-        King WK = new King();
-        Position positionWK = new Position();
-        positionWK = position[]
+
+        King WK = new King(); // Nouvelle pièce Roi
+        Position positionWK = new Position(); // Nouvelle instance de position pour cette pièce
+        positionWK.init('e', 1); // On définit la position du roi en début de partie
         WK.init(positionWK, 0);
+        board[7][4].setPiece(WK);
 
         Queen WQ = new Queen();
         Position positionWQ = new Position();
         positionWQ.init('d', 1);
         WQ.init(positionWQ, 0);
-        d1.setPiece(WQ);
-        board[7][3] = d1;
+        board[7][3].setPiece(WQ);
 
         Pawn[] WP = new Pawn[8];
         Position[] positionWP = new Position[8];
         columnChar = 'a';
         for (int i = 0; i <= 7; i++) {
+            WP[i] = new Pawn();
+            positionWP[i] = new Position();
             positionWP[i].init(columnChar, 2);
             WP[i].init(positionWP[i], 0);
             board[6][i].setPiece(WP[i]);
             columnChar += 1;
         }
 
-        Bishop[] WB = new Bishop[2];
-        Position[] positionWB = new Position[2];
+        Bishop WB1 = new Bishop();
+        Bishop WB2 = new Bishop();
+        Position positionWB1 = new Position();
+        Position positionWB2 = new Position();
 
-        positionWB[0].init('c', 1);
-        WB[0].init(positionWB[0], 0);
-        board[7][2].setPiece(WB[0]);
+        positionWB1.init('c', 1);
+        WB1.init(positionWB1, 0);
+        board[7][2].setPiece(WB1);
 
-        positionWB[1].init('f', 1);
-        WB[1].init(positionWB[1], 0);
-        board[7][5].setPiece(WB[1]);
-
-
-        Knight[] WN = new Knight[2];
-        Position[] positionWN = new Position[2];
-
-        positionWN[0].init('b', 1);
-        WN[0].init(positionWN[0], 0);
-        board[7][1].setPiece(WN[0]);
-
-        positionWN[1].init('g', 1);
-        WN[1].init(positionWN[1], 0);
-        board[7][6].setPiece(WN[1]);
+        positionWB2.init('f', 1);
+        WB1.init(positionWB2, 0);
+        board[7][5].setPiece(WB2);
 
 
-        Rook[] WR = new Rook[2];
-        Position[] positionWR = new Position[2];
+        Knight WN1 = new Knight();
+        Knight WN2 = new Knight();
+        Position positionWN1 = new Position();
+        Position positionWN2 = new Position();
 
-        positionWR[0].init('a', 1);
-        WR[0].init(positionWR[0], 0);
-        board[7][0].setPiece(WR[0]);
+        positionWN1.init('b', 1);
+        WN1.init(positionWN1, 0);
+        board[7][1].setPiece(WN1);
 
-        positionWR[1].init('h', 1);
-        WR[1].init(positionWR[1], 0);
-        board[7][7].setPiece(WR[1]);
+        positionWN2.init('g', 1);
+        WN2.init(positionWN2, 0);
+        board[7][6].setPiece(WN2);
+
+
+        Rook WR1 = new Rook();
+        Rook WR2 = new Rook();
+        Position positionWR1 = new Position();
+        Position positionWR2 = new Position();
+
+        positionWR1.init('a', 1);
+        WR1.init(positionWR1, 0);
+        board[7][0].setPiece(WR1);
+
+        positionWR2.init('h', 1);
+        WR2.init(positionWR2, 0);
+        board[7][7].setPiece(WR2);
 
         //Initialisation des pièces noires
         King BK = new King();
@@ -130,57 +148,59 @@ public class Chess {
         Position positionBQ = new Position();
         positionBQ.init('d', 1);
         BQ.init(positionBQ, 0);
-        Cell d1 = new Cell();
-        d1.setPiece(BQ);
-        board[7][3] = d1;
+        board[7][3].setPiece(BQ);
 
         Pawn[] BP = new Pawn[8];
         Position[] positionBP = new Position[8];
         columnChar = 'a';
         for (int i = 0; i <= 7; i++) {
+            BP[i] = new Pawn();
+            positionBP[i] = new Position();
             positionBP[i].init(columnChar, 2);
             BP[i].init(positionBP[i], 0);
             board[6][i].setPiece(BP[i]);
             columnChar += 1;
         }
 
-        Bishop[] BB = new Bishop[2];
-        Position[] positionBB = new Position[2];
+        Bishop BB1 = new Bishop();
+        Bishop BB2 = new Bishop();
+        Position positionBB1 = new Position();
+        Position positionBB2 = new Position();
 
-        positionBB[0].init('c', 1);
-        BB[0].init(positionBB[0], 0);
-        board[7][2].setPiece(BB[0]);
+        positionBB1.init('c', 1);
+        BB1.init(positionBB1, 0);
+        board[7][2].setPiece(BB1);
 
-        positionBB[1].init('f', 1);
-        BB[1].init(positionBB[1], 0);
-        board[7][5].setPiece(BB[1]);
-
-
-        Knight[] BN = new Knight[2];
-        Position[] positionBN = new Position[2];
-
-        positionBN[0].init('b', 1);
-        BN[0].init(positionBN[0], 0);
-        board[7][1].setPiece(BN[0]);
-
-        positionBN[1].init('g', 1);
-        BN[1].init(positionBN[1], 0);
-        board[7][6].setPiece(BN[1]);
+        positionBB2.init('f', 1);
+        BB2.init(positionBB2, 0);
+        board[7][5].setPiece(BB2);
 
 
-        Rook[] BR = new Rook[2];
-        Position[] positionBR = new Position[2];
+        Knight BN1 = new Knight();
+        Knight BN2 = new Knight();
+        Position positionBN1 = new Position();
+        Position positionBN2 = new Position();
 
-        positionBR[0].init('a', 1);
-        BR[0].init(positionBR[0], 0);
-        board[7][0].setPiece(BR[0]);
+        positionBN1.init('b', 1);
+        BN1.init(positionBN1, 0);
+        board[7][1].setPiece(BN1);
 
-        positionBR[1].init('h', 1);
-        BR[1].init(positionBR[1], 0);
-        board[7][7].setPiece(BR[1]);
+        positionBN2.init('g', 1);
+        BN2.init(positionBN2, 0);
+        board[7][6].setPiece(BN2);
 
+        Rook BR1 = new Rook();
+        Rook BR2 = new Rook();
+        Position positionBR1 = new Position();
+        Position positionBR2 = new Position();
 
+        positionBR1.init('a', 1);
+        BR1.init(positionBR1, 0);
+        board[7][0].setPiece(BR1);
 
+        positionBR2.init('h', 1);
+        BR2.init(positionBR2, 0);
+        board[7][7].setPiece(BR2);
     }
 
     private void printBoard() {
